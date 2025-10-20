f9518aff3e7286d726e37dbc15894c131e93a80b
osvaldocs <pablo_campos92@hotmail.com.ar>
2025-10-20 13:29:29 -0500

    subir curso introductorio

---
+++ src/main/java/com/example/demo/HolaController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/hola")
+    public String saludar() {
+        return "¡Hola desde Spring Boot!";
+    }
+}
+
