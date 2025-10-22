7065e9ab38ec41c785740521600fcbbfc7774643
osvaldocs <pablo_campos92@hotmail.com.ar>
2025-10-21 19:53:52 -0500

    Primer commit en tiniTask

--- src/main/java/com/example/demo/model/Todo.java
+++ src/main/java/com/example/demo/model/Todo.java
@@ -1,4 +1,42 @@
 package com.example.demo.model;
 
 public class Todo {
+    private int id;
+    private String title;
+    private boolean done;
+
+    public Todo() {
+    }
+
+    public Todo(int id, String title, boolean done) {
+        this.id = id;
+        this.title = title;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
+
+
--- src/main/java/com/example/demo/repository/TodoRepository.java
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -4,10 +4,53 @@ import com.example.demo.model.Todo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class TodoRepository {
 
+    private final List<Todo> todoList = new ArrayList<>();
+    private int nextId = 1;
+
     public List<Todo> getAll() {
-       return new ArrayList<>();
+       return todoList;
     }
+
+    public void add(Todo todo) {
+        todo.setId(nextId);
+        nextId++;
+        todoList.add(todo);
+    }
+
+    public Optional<Todo> findById(int id) {
+        for (Todo todo : todoList) {
+            if (todo.getId() == id) {
+                return Optional.of(todo);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public boolean deleteById(int id) {
+       Optional<Todo> todoFound = findById(id);
+
+       if (todoFound.isPresent()) {
+           todoList.remove(todoFound.get());
+           return true;
+       }
+       return false;
+    }
+
+    public boolean update(int id, String newTitle, boolean newDone) {
+        Optional<Todo> todoFound = findById(id);
+
+        if (todoFound.isPresent()) {
+            Todo todo = todoFound.get();
+            todo.setTitle(newTitle);
+            todo.setDone(newDone);
+
+            return true;
+        }
+        return false;
+    }
+    
 }
