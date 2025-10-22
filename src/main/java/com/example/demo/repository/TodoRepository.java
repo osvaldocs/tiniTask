8ff0f386be828530521d1231c975d9c1818b5aa6
osvaldocs <pablo_campos92@hotmail.com.ar>
2025-10-21 19:48:58 -0500

    tiniTask

---
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoRepository {
+
+    public List<Todo> getAll() {
+       return new ArrayList<>();
+    }
+}
