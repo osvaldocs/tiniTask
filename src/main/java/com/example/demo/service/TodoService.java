dfb1d59b279a5716e97fb9fc7bf24bf22e47d981
xguerrerov0903 <xguerrerov0903@gmail.com>
2025-10-22 13:46:48 -0500

    service

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,4 +1,40 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Todo;
+import com.example.demo.repository.TodoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TodoService {
+
+    private final TodoRepository repository;
+
+    public TodoService(TodoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Todo create(String title) {
+        if (title == null || title.trim().length() < 3) {
+            throw new IllegalArgumentException("Title is required and must have at least 3 characters");
+        }
+        Todo todo = new Todo();
+        todo.setTitle(title);
+        todo.setDone(false);
+        repository.add(todo);
+        return todo;
+    }
+
+    public List<Todo> getAll() {
+        return repository.getAll();
+    }
+
+    public Optional<Todo> toggleDone(int id) {
+        Optional<Todo> todoOpt = repository.findById(id);
+
+        if (todoOpt.isPresent())
+    }
 }
